3e7d8fad4208eb227839c6d910721816f1864b4a
Rhiski Aprilianto <riskey95@gmail.com>
2014-10-10 16:59:33 +0700

    cpufreq: lagfree: remove lagfree governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -382,9 +382,6 @@ extern struct cpufreq_governor cpufreq_gov_lulzactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
-extern struct cpufreq_governor cpufreq_gov_lagfree;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lagfree)
 #endif
 
 

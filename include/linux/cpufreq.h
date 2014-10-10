455094de78800c096041960a5223e4e5cd9c6c6b
Rhiski Aprilianto <riskey95@gmail.com>
2014-10-10 19:05:28 +0700

    cpufreq: add pegasusq governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -400,6 +400,9 @@ extern struct cpufreq_governor cpufreq_gov_alucard;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
 extern struct cpufreq_governor cpufreq_gov_HYPER;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_HYPER)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
+extern struct cpufreq_governor cpufreq_gov_pegasusq;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
 #endif
 
 
